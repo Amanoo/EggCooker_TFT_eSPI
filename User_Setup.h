2c0cbaa5f85c03cf3c8bd494e78458a241ca2717
Amanoo <marcoxmeijer@gmail.com>
2023-03-07 02:24:12 +0100

    Disable touch (for use with different library)

--- User_Setup.h
+++ User_Setup.h
@@ -221,7 +221,7 @@
 //#define TFT_RST  33  // Reset pin (could connect to Arduino RESET pin)
 #define TFT_BL   26  // LED back-light
 
-#define TOUCH_CS 12     // Chip select pin (T_CS) of touch screen
+//#define TOUCH_CS 12     // Chip select pin (T_CS) of touch screen
 
 //#define TFT_WR 22    // Write strobe for modified Raspberry Pi TFT only
 
